60cecefc9bc22c0183af66c3e08cb5eb45a263b9
denis lussier <denis@lussier.io>
2022-07-01 03:06:06 +0000

    freshen up the IN s3 scripts

--- devel/util/in/dryrun.sh
+++ devel/util/in/dryrun.sh
@@ -1,11 +1,4 @@
-
-
-if [ ! "$1" == "prod" ]; then
-  echo "invalid 1st parm"
-  exit 1
-fi
-
-echo "push-s3 $1 dryrun..."
-./push-s3.sh $1 --dryrun
-echo "pull-s3 $1 dryrun..."
-./pull-s3.sh $1 --dryrun
+echo "push-s3 dryrun..."
+./push-s3.sh --dryrun
+echo "pull-s3 dryrun..."
+./pull-s3.sh --dryrun
--- devel/util/in/push-s3.sh
+++ devel/util/in/push-s3.sh
@@ -1,11 +1,5 @@
 
-if [ ! "$1" == "prod" ]; then
-  echo "invalid 1st parm"
-  exit 1
-fi
-
-
-cmd="aws --region $REGION s3 sync . $BUCKET/IN $2 $3"
+cmd="aws --region $REGION s3 sync . $BUCKET/IN $1 $2"
 echo $cmd
 sleep 3
 
