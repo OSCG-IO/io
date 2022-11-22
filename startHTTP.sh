dea6c91b6a0573cf290274dd34fb8e39196d6ae7
denis lussier <denis@lussier.io>
2022-11-22 20:16:53 +0000

    refactor (re)startHTTP.sh

--- startHTTP.sh
+++ startHTTP.sh
@@ -1,15 +1,6 @@
-pyver=`python3 --version  > /dev/null 2>&1`
-rc=$?
-if [ "$rc" == "0" ]; then
-  pid=`ps aux | grep [h]ttp.server | awk '{print $2}'`
-  if [ "$pid" > " " ]; then
-    echo "killing ($pid)"
-    kill -9 $pid > /dev/null 2>&1
-  fi
-  cmd="python3 -m http.server"
-fi
 
-rm ' '
+./stopHTTP.sh
+ cmd="python3 -m http.server"
 echo $cmd
 cd $OUT
 $cmd &
