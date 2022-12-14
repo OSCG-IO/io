6e91cd1717e424bc977fbe8efce0bbb1b72aa1a1
denis lussier <denis@lussier.io>
2022-12-14 19:44:26 +0000

    1st pass add swagger to postgrest

--- src/postgrest/start-postgrest.py
+++ src/postgrest/start-postgrest.py
@@ -1,9 +1,15 @@
+
 import os
 import util
+
+logdir = os.getcwd() + "/data/logs/postgrest"
 
 port = util.get_comp_port("postgrest")
 
 print("postgrest starting on port " + port)
+os.system("postgrest/postgrest postgrest/postgrest.conf >> " + logdir + "/postgrest.log 2>&1 &")
 
-os.system("postgrest/postgrest postgrest/postgrest.conf >> data/logs/postgrest/postgrest.log 2>&1 &")
+print("swagger starting on port 8080")
+os.chdir("/usr/local/lib/node_modules/swagger-ui-dist/")
+os.system("npx http-server >> " + logdir + "/swagger.log 2>&1 &")
 
