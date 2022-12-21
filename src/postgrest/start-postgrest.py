2f3503db87f6ef308abfd2f284835b79c4385e46
denis lussier <denis@lussier.io>
2022-12-21 06:13:52 +0000

    remove swagger from postgrest install

--- src/postgrest/start-postgrest.py
+++ src/postgrest/start-postgrest.py
@@ -13,7 +13,7 @@ os.environ['PGREST_SERVER_HOST'] = PGREST_SERVER_HOST
 print("postgrest starting on " + PGREST_SERVER_HOST + ":" + str(port))
 os.system("postgrest/postgrest postgrest/postgrest.conf >> " + logdir + "/postgrest.log 2>&1 &")
 
-print("swagger starting on port 8080")
-os.chdir("/usr/local/lib/node_modules/swagger-ui-dist/")
-os.system("npx http-server >> " + logdir + "/swagger.log 2>&1 &")
+##print("swagger starting on port 8080")
+##os.chdir("/usr/local/lib/node_modules/swagger-ui-dist/")
+##os.system("npx http-server >> " + logdir + "/swagger.log 2>&1 &")
 
